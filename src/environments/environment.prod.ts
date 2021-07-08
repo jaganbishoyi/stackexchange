bde75e374aa1b4bfda487644f03e4de2d34b5cfd
Jagan Mohan Bishoyi <jagan.bishoyi@people10.com>
2021-07-09 00:15:43 +0530

    Demo added

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  SERVER_URL: 'https://api.stackexchange.com/2.2/',
-  API_ENDPOINT: 'https://api.stackexchange.com/2.2/'
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  SERVER_URL: 'https://api.stackexchange.com/2.2/',
-  API_ENDPOINT: 'https://api.stackexchange.com/2.2/',
-};
-
